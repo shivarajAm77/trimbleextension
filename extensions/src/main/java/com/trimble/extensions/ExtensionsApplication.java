2f62604c7b4c1fadd25df7bd03118286d1ec867e
shivaraj <shivaraj.am@virtuele.us>
2025-10-17 15:24:46 +0530

    Initial commit

---
+++ extensions/src/main/java/com/trimble/extensions/ExtensionsApplication.java
@@ -0,0 +1,13 @@
+package com.trimble.extensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionsApplication.class, args);
+	}
+
+}
