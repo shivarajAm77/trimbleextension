8ac5af0e3c04698a3342ebf9d81740591afecb80
shivarajAm77 <137749471+shivarajAm77@users.noreply.github.com>
2026-01-12 12:11:05 +0530

    Update ExtensionsApplicationTests.java

--- extensions/src/test/java/com/trimble/extensions/ExtensionsApplicationTests.java
+++ extensions/src/test/java/com/trimble/extensions/ExtensionsApplicationTests.java
@@ -3,11 +3,9 @@ package com.trimble.extensions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ExtensionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
